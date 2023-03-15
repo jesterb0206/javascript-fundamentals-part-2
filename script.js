f37488434a1ca085dd762e14d47ed13f2e911727
jesterb0206 <jesterb@seattleu.edu>
2023-03-15 16:56:01 -0700

    Activating Strict Mode

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasALicense = false;
+const passedTest = true;
+
+if (passedTest) hasALicense = true;
+if (hasALicense) console.log('I can drive!');
+
+// const interface = 'Audio';
+// const private = 123;
+// const if = 456;
