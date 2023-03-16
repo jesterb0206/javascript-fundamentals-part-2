8e74480e51aab7cb4b710b883be2a70575a8fbd3
jesterb0206 <jesterb@seattleu.edu>
2023-03-15 20:12:01 -0700

    Basic Array Operations (Methods)

--- script.js
+++ script.js
@@ -107,46 +107,70 @@
 
 // checkWinner(dolphinScore, koalaScore);
 
+// const friends = ['Haneia', 'Noah', 'Rosa ❤️'];
+
+// console.log(friends);
+
+// const years = new Array(1969, 1970, 1997, 2000, 2001);
+
+// console.log(friends[0]);
+
+// console.log(friends.length);
+
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Rosalinda ❤️';
+
+// console.log(friends);
+
+// const bradley = ['Bradley', 'Jester', '26', 'Full Stack Developer', friends];
+
+// console.log(bradley);
+
+// const calcAge = function (birthYear) {
+//   return 2023 - birthYear;
+// };
+
+// const birthYears = [1969, 1970, 1997, 2000, 2001];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[2]);
+// const age4 = calcAge(years[3]);
+// const age5 = calcAge(years[birthYears.length - 1]);
+
+// console.log(age1, age2, age3, age4, age5);
+
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[2]),
+//   calcAge(years[3]),
+//   calcAge(years[birthYears.length - 1]),
+// ];
+
+// console.log(ages);
+
 const friends = ['Haneia', 'Noah', 'Rosa ❤️'];
+const newLength = friends.push('Zachary');
+console.log(friends);
+console.log(newLength);
 
+friends.unshift('Daniela');
 console.log(friends);
 
-const years = new Array(1969, 1970, 1997, 2000, 2001);
-
-console.log(friends[0]);
-
-console.log(friends.length);
-
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Rosalinda ❤️';
-
+const pop = friends.pop();
+console.log(pop);
 console.log(friends);
 
-const bradley = ['Bradley', 'Jester', '26', 'Full Stack Developer', friends];
+friends.shift();
+console.log(friends);
 
-console.log(bradley);
+console.log(friends.indexOf('Noah'));
 
-const calcAge = function (birthYear) {
-  return 2023 - birthYear;
-};
+console.log(friends.includes('Noah'));
+console.log(friends.includes('Ben'));
 
-const birthYears = [1969, 1970, 1997, 2000, 2001];
-
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[2]);
-const age4 = calcAge(years[3]);
-const age5 = calcAge(years[birthYears.length - 1]);
-
-console.log(age1, age2, age3, age4, age5);
-
-const ages = [
-  calcAge(years[0]),
-  calcAge(years[1]),
-  calcAge(years[2]),
-  calcAge(years[3]),
-  calcAge(years[birthYears.length - 1]),
-];
-
-console.log(ages);
+if (friends.includes('Haneia')) {
+  console.log('Bradley has a friend named Haneia!');
+}
