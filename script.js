082caaeb022fddc1ba0779d79a2129ff6b33ef64
jesterb0206 <jesterb@seattleu.edu>
2023-03-17 19:58:00 -0700

    Coding Challenge #4

--- script.js
+++ script.js
@@ -363,20 +363,52 @@
 //   }
 // }
 
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Bench press repetition #${rep} 🏋️‍♀️`);
-}
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Bench press repetition #${rep} 🏋️‍♀️`);
+// }
 
-let rep = 1;
+// let rep = 1;
 
-while (rep <= 10) {
-  rep++;
-}
+// while (rep <= 10) {
+//   rep++;
+// }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
+// let dice = Math.trunc(Math.random() * 6) + 1;
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log('Finished!');
-}
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log('Finished!');
+// }
+
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+// const tips = [];
+
+// const totals = [];
+
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
+
+// console.log(bills, tips, totals);
+
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
+
+// console.log(calcAverage([2, 3, 7]));
+
+// console.log(calcAverage(totals));
+
+// console.log(calcAverage(tips));
